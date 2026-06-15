15af6700dba6b1cf57f79cd01d4c44a81bde3bf4
davidleon2026 <david.brian.leon@estudiantat.upc.edu>
2026-06-16 00:04:30 +0200

    texto flotante para feedback visual

--- view.py
+++ view.py
@@ -19,4 +19,28 @@ CITY_W  = SCREEN_W - SIDEBAR_W
 CITY_H  = SCREEN_H - TOPBAR_H
 SIDEBAR_X = CITY_W
 
+class FloatingText:
+    """Texto flotante animado para feedback visual. Es visual."""
+
+    def __init__(self, text, pos, color):
+        self.text  = text
+        self.x, self.y = pos
+        self.color = color
+        self.life  = 1.5
+        self.alpha = 255
+
+    def update(self, dt):
+        self.life -= dt
+        self.y    -= 35 * dt
+        self.alpha = max(0, int(255 * (self.life / 1.5)))
+
+
+    def alive(self):
+        return self.life > 0
+
+    def draw(self, surf):
+        f = font(14, bold=True)
+        rendered = f.render(self.text, True, self.color)
+        rendered.set_alpha(self.alpha)
+        surf.blit(rendered, (int(self.x), int(self.y)))
 
